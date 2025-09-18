5a393ada72bc21c2ad201daaafce1ad67f9d81f3
aditya-2k23 <holaaditya123@gmail.com>
2025-09-18 20:13:49 +0530

    Refactor layout components for consistent styling and improved readability

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,7 @@ import "./global.css";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-4xl font-bold">Hello</Text>
     </View>
   );
